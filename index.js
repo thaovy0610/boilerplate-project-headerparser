5c5c2b2493b3e08443bd71c083c90b153308959c
thaovy0610 <thaovy0610@gmail.com>
2024-11-26 17:59:27 +0700

    update API endpoint

--- index.js
+++ index.js
@@ -20,10 +20,18 @@ app.get('/', function (req, res) {
 });
 
 // your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
+app.get('/api/whoami', function (req, res) {
+  const headers = req.headers
+  const ipAddress = req.ip
+  const language = headers['accept-language']
+  const software = headers['user-agent']
+  
+  res.json({ 
+    ipaddress: ipAddress,
+    language: language,
+    software: software
+  });
 });
-
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
